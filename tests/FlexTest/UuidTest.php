5c787d28b4f27e2ef69207f0f99472843f9bd2cc
elnebuloso <jan.thoennessen@googlemail.com>
2014-09-24 23:01:45 +0200

    release

--- tests/FlexTest/UuidTest.php
+++ tests/FlexTest/UuidTest.php
@@ -12,18 +12,11 @@ use Flex\Uuid;
 class UuidTest extends \PHPUnit_Framework_TestCase {
 
     /**
-     * @return void
-     */
-    public function test_uuidLength() {
-        $this->assertEquals(36, strlen(Uuid::get()), 'strlen of uuid must be 36');
-    }
-
-    /**
      * c851db2f-f7b2-4e4f-9ea0-343f592f4cb7
      *
-     * @return void
+     * @test
      */
-    public function test_uuidPattern() {
+    public function uuidPattern() {
         $pattern = '`([a-f0-9]{8})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{12})`';
         $this->assertRegExp($pattern, Uuid::get(), 'uuid does not match the pattern');
     }
