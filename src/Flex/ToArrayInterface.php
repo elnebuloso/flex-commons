93431e5b44f8d60d64166f318b83a4cfa9a939d5
elnebuloso <jan.thoennessen@googlemail.com>
2014-11-19 21:57:53 +0100

    added collection added Interfaces

---
+++ src/Flex/ToArrayInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Flex;
+
+/**
+ * Class ToArrayInterface
+ *
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ */
+interface ToArrayInterface {
+
+    /**
+     * @return array
+     */
+    public function toArray();
+}
---
+++ src/Flex/ToJsonInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Flex;
+
+/**
+ * Class ToJsonInterface
+ *
+ * @author Jeff Tunessen <jeff.tunessen@gmail.com>
+ */
+interface ToJsonInterface {
+
+    /**
+     * @return array
+     */
+    public function toJson();
+}
